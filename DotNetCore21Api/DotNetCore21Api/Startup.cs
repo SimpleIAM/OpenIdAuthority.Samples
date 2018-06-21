63f734853f03401c6249541c0de7745e161430d2
Ryan Foster <ryan@openidconnect.com>
2018-06-20 18:42:17 -0700

    Update and add samples

---
+++ DotNetCore21Api/DotNetCore21Api/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DotNetCore21Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options => 
+            {
+                options.Authority = Configuration["Authentication:Authority"];
+                options.Audience = Configuration["Authentication:Audience"];
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
---
+++ DotNetCoreConsoleApp/DotNetCoreConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace DotNetCoreConsoleApp
+{
+    // Illustrates client credentials flow to get access to an api
+    class Program
+    {
+        private const string authority = "https://localhost:5101";
+        private const string scopes = "demoapi-read";
+        private const string api = "https://localhost:5105";
+        private const string clientId = "dotnetcorebackendservice";
+        private const string clientSecret = "put dev secret in user secrets file";
+
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            DiscoveryResponse authorityMetadata;
+            try
+            {
+                authorityMetadata = await DiscoveryClient.GetAsync(authority);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+            if(authorityMetadata.IsError)
+            {
+                Console.WriteLine(authorityMetadata.Error);
+                return;
+            }
+
+            var tokenFetcher = new TokenClient(authorityMetadata.TokenEndpoint, clientId, clientSecret);
+            var response = await tokenFetcher.RequestClientCredentialsAsync(scopes);
+            if(response.IsError)
+            {
+                Console.WriteLine(response.Error);
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine(response.Json);
+
+
+            string responseText;
+            try
+            {
+                responseText = await api
+                    .AppendPathSegments("api", "values")
+                    .WithOAuthBearerToken(response.AccessToken)
+                    .GetStringAsync();
+            }
+            catch (FlurlHttpException ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+
+            Console.WriteLine("Response: ", responseText);
+            Console.ReadKey();
+        }
+    }
+}
